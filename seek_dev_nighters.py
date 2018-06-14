8fd14c0d099ac73e10021696057d484ce4c4aff5
ЭФкудфМЭ <arelav90@mail.ru>
2018-06-14 23:41:32 +0300

    add url read

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,11 +3,15 @@ import pytz
 
 
 def load_attempts():
-    url = 'https://devman.org/api/challenges/solution_attempts/?page=1'
-    for page in range(2):
-        response_data = requests.get(url).json()
-        for data in response_data['records']:
-            yield data
+    main_url = 'https://devman.org/api/challenges/solution_attempts/'
+    response_data = requests.get(main_url).json()
+    count_of_pages = response_data['number_of_pages']
+    for page in range(count_of_pages + 1):
+        url = main_url + '?page=' + str(page)
+        print(response_data['records'])
+    #     response_data = requests.get(url).json()
+    #     for data in response_data['records']:
+    #         yield data
 
 
 def get_midnighters():
@@ -16,4 +20,3 @@ def get_midnighters():
 
 if __name__ == '__main__':
     info = load_attempts()
-    print(info)
