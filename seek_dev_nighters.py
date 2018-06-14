abe6b0e4427049e8d6b5ad39deb3478fd30d2cdf
maltsev <arelav90@mail.ru>
2018-06-14 17:31:28 +0300

    first try to load data

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,19 @@
+import requests
+import pytz
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    url = 'https://devman.org/api/challenges/solution_attempts/?page=1'
+    for page in range(2):
+        response_data = requests.get(url).json()
+        for data in response_data['records']:
+            yield data
+
 
 def get_midnighters():
     pass
 
+
 if __name__ == '__main__':
-  pass
+    info = load_attempts()
+    print(info)
