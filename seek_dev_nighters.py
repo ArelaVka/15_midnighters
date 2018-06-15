634586d327ebd618698e864b4f0224e8a02aaafb
maltsev <arelav90@mail.ru>
2018-06-15 12:29:03 +0300

    add time output

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 import pytz
 
 
@@ -6,17 +7,25 @@ def load_attempts():
     main_url = 'https://devman.org/api/challenges/solution_attempts/'
     response_data = requests.get(main_url).json()
     count_of_pages = response_data['number_of_pages']
-    for page in range(count_of_pages + 1):
+    for page in range(1, count_of_pages + 1):
         url = main_url + '?page=' + str(page)
-        print(response_data['records'])
-    #     response_data = requests.get(url).json()
-    #     for data in response_data['records']:
-    #         yield data
+        page_records_data = requests.get(url).json()
+        for record in page_records_data['records']:
+            yield record
 
 
-def get_midnighters():
-    pass
+def get_midnighters(record):
+    if record['timestamp']:
+        return datetime.datetime.fromtimestamp(
+            record['timestamp'],
+            pytz.timezone(record['timezone'])).time()
+        # upload_time = datetime.date.fromtimestamp(record['timestamp'])
 
 
 if __name__ == '__main__':
     info = load_attempts()
+    out = set()
+    for info in load_attempts():
+        print(get_midnighters(info))
+        # out.add(info['username'])
+        # print(out)
